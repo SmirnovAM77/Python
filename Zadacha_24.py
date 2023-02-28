0f81c84da9379b0fd68347feca65def9adb78c82
artem <temik-cool@mail.ru>
2023-02-28 20:37:24 +0300

    Задача_24_2

--- Zadacha_24.py
+++ Zadacha_24.py
@@ -4,7 +4,14 @@
 #4 -> 1 2 3 4
 #9
 
-n = int(input("Введите количество кустов: "))
+n = int(input("Введите количество кустов (минимум 3 куста на ферме): "))
 from random import randint 
 list1 = [randint(1, n) for i in range(n)]
 print(*list1)
+k = 0
+maxModul = 0
+for i in range(1, len(list1) - 1):
+    k = list1[i] + list1[i - 1] + list1[i + 1]
+    if k > maxModul:
+        maxModul = k
+print(maxModul)
