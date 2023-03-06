2455980bdb12e04b49d8153f2f32ddae0b49ea7e
artem <temik-cool@mail.ru>
2023-03-06 21:54:03 +0300

    Задача_30

--- Zadacha_28.py
+++ Zadacha_28.py
@@ -10,16 +10,8 @@ a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 if a < b:
     a, b = b, a
-print(a, b)
 def sum(a, b):
     if b == 0:
         return a
     return sum(a + 1, b - 1)
 print(sum(a, b))   
-
-#deg = a              
-#def degree(a, b):
-#    if b == 1:
-#        return a
-#    return degree(a * deg, b - 1)
-#print(degree(a, b))
